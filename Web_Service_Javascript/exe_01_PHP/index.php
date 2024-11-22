7f3ff2b6742e9bd39f8bc0a8c4a8be0cde5f9bbe
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2024-11-22 09:58:13 +0100

    DLH

--- Web_Service_Javascript/exe_01_PHP/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo file_get_contents("https://www.bbc.com/weather/2960316");
-?>
--- Web_Service_Javascript/exe_01_PHP/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(init)//.ready(init) quando a funcao document estiver pronta, ira chamar a variavel init
-function init(){
-    $("#btn").click(getWeather);//getWeather function depois de clicar em button <button onclick="getWeather()"></button>
-}
- 
-function getWeather(){
-    $("#output").load("index.php .wr-value--temperature--c:first");//.load() carrega os dados de um servidor e coloca no elemento selecionado
-}
